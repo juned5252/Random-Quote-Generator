7f4ec233be78de669ca46332231fc1405341c4a8
Juned Alam <juned5252@gmail.com>
2018-06-09 02:45:31 -0400

    added working functions PrintQuote() and getRandomQuote()

---
+++ js/quotesArray.js
@@ -0,0 +1,64 @@
+
+
+// this file contains the array of quote objects that will be referenced by the script.js file
+
+var quotes = [
+
+  {
+    quote:'In a gentle way, you can shake the world.',
+    source:"— Mahatma Gandhi",
+    year:1930
+  },
+
+  {
+    quote:"Don’t ask yourself what the world needs. Ask yourself what makes you come alive and then go do that. Because what the world needs is people who have come alive.",
+    source:"— Howard Thurman"
+  },
+
+  {
+    quote:'The most authentic thing about us is our capacity to create, to overcome,to endure, to transform, to love and to be greater than our suffering.',
+    source:"— Ben Okri" ,
+    year:2001
+  },
+
+  {
+    quote:'If you hear a voice within you say ‘you cannot paint,’then by all means paint and that voice will be silenced.',
+     source:"- Vincent Van Gogh" ,
+     year:1870
+  },
+
+  {
+    quote:'The only person you are destined to become is the person you decide to be.',
+    source:" - Ralph Waldo Emerson"  ,
+    year:1850
+  },
+
+  {
+    quote:'Fall seven times and stand up eight.',
+    source: "- Japanese Proverb"
+  },
+
+  {
+    quote:'The person who says it cannot be done should not interrupt the person who is doing it.',
+    source:"- Chinese Proverb"
+  },
+
+  {
+    quote:'It does not matter how slowly you go as long as you do not stop.',
+    source:"- Confucius ",
+  },
+
+  {
+    quote:'Perfection is not attainable, but if we chase perfection we can catch excellence.',
+    source:"— Vince Lombardi ",
+    year:1950
+  },
+
+  {
+    quote:"If a lot of people love each other, the world would be a better place to live.",
+    source:"Tommy Wiseau",
+    citation:"The Room",
+    year:2001
+  }
+
+]
--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
+var message = ''
+
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+
+// getRandomQuote returns a random quote object from the quotesArray
+function getRandomQuote(){
+  var rand = Math.floor(Math.random() * quotes.length);
+  var output = quotes[rand];
+  return output;
+}
+
+// printQuote builds a string from the getRandomQuote function and adds it to the DOM
+function printQuote(){
+  var result = getRandomQuote();
+  message = '<p class="quote" >' + result.quote + '</p>';
+  document.getElementById('quote-box').innerHTML = message;
+  //console.log(message);
+}
+/*var index
+
+for(var i = 0; i < quotes.length; i++){
+  index = quotes[i];
+  for(var key in index){
+  console.log(index[key]);
+  }
+}
+*/
