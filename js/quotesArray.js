5ef5755cd902e20e0904496589390f4477d1728e
Juned Alam <juned5252@gmail.com>
2018-06-09 23:53:01 -0400

    added image property to objects that changes background image

--- js/quotesArray.js
+++ js/quotesArray.js
@@ -6,59 +6,70 @@ var quotes = [
 
   {
     quote:'In a gentle way, you can shake the world.',
-    source:"— Mahatma Gandhi",
-    year:1930
+    source:"Mahatma Gandhi",
+    year:1930,
+    img:'https://images.unsplash.com/photo-1456750695228-762f8db965a4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5d243620cb6b39937ec5e1f0785ffa46&auto=format&fit=crop&w=1651&q=80'
   },
 
   {
-    quote:"Don’t ask yourself what the world needs. Ask yourself what makes you come alive and then go do that. Because what the world needs is people who have come alive.",
-    source:"— Howard Thurman"
+    quote:"There is only one way to avoid criticism: do nothing, say nothing, and be nothing.",
+    source:"Aristotle",
+    img:'https://images.unsplash.com/photo-1507255782351-45654f0c20d1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ed21edd21a4192484cf036731d2275d4&auto=format&fit=crop&w=1650&q=80'
   },
 
   {
-    quote:'The most authentic thing about us is our capacity to create, to overcome,to endure, to transform, to love and to be greater than our suffering.',
-    source:"— Ben Okri" ,
-    year:2001
+    quote:'How lucky I am to have something that makes saying goodbye so hard.',
+    source:" A.A. Milne",
+    citation:"The Complete Tales of Winnie-the-Pooh",
+    year:1926,
+    img:'https://images.unsplash.com/uploads/1413142095961484763cf/d141726c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=75bbfe920b2640aa7806b3bc3286af86&auto=format&fit=crop&w=1650&q=80'
   },
 
   {
     quote:'If you hear a voice within you say ‘you cannot paint,’then by all means paint and that voice will be silenced.',
-     source:"- Vincent Van Gogh" ,
-     year:1870
+     source:"Vincent Van Gogh",
+     year:1870,
+     img:'https://images.unsplash.com/photo-1477696957384-3b1d731c4cff?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=27463acb8518d4e6819d6f4441c8e47f&auto=format&fit=crop&w=1650&q=80'
   },
 
   {
     quote:'The only person you are destined to become is the person you decide to be.',
-    source:" - Ralph Waldo Emerson"  ,
-    year:1850
+    source:" Ralph Waldo Emerson",
+    year:1850,
+    img:'https://images.unsplash.com/photo-1431794062232-2a99a5431c6c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=13b58b0343d8efc06a88c55e843f624f&auto=format&fit=crop&w=1650&q=80'
   },
 
   {
     quote:'Fall seven times and stand up eight.',
-    source: "- Japanese Proverb"
+    source: "Japanese Proverb",
+    img:'https://images.unsplash.com/photo-1432634372475-07b399e372ee?ixlib=rb-0.3.5&s=486cb46994699d49b71122001fe57583&auto=format&fit=crop&w=2550&q=80'
   },
 
   {
     quote:'The person who says it cannot be done should not interrupt the person who is doing it.',
-    source:"- Chinese Proverb"
+    source:"Chinese Proverb",
+    img:'https://images.unsplash.com/photo-1474524955719-b9f87c50ce47?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c83ef66968ed8c6a5c1ac1111ee78c1e&auto=format&fit=crop&w=1652&q=80'
   },
 
   {
     quote:'It does not matter how slowly you go as long as you do not stop.',
-    source:"- Confucius ",
+    source:"Confucius ",
+    img:'https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjE2ODQ0fQ&s=213dd07f633707084321fdeb02f8e192&auto=format&fit=crop&w=1650&q=80'
   },
 
   {
     quote:'Perfection is not attainable, but if we chase perfection we can catch excellence.',
-    source:"— Vince Lombardi ",
-    year:1950
+    source:"Vince Lombardi ",
+    year:1950,
+    img:'https://images.unsplash.com/photo-1442876906995-6761040d1f0b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3f298a52eb789d2258812c601ad22962&auto=format&fit=crop&w=1650&q=80'
   },
 
   {
     quote:"If a lot of people love each other, the world would be a better place to live.",
     source:"Tommy Wiseau",
     citation:"The Room",
-    year:2001
+    year:2001,
+    img:'https://images.unsplash.com/photo-1470940383688-5eae6a3d940c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e89515675009077c9d74d204214d1e1a&auto=format&fit=crop&w=1314&q=80'
   }
 
 ]
--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 var message = ''
-
+var result
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
 
 
 // getRandomQuote returns a random quote object from the quotesArray
@@ -14,17 +15,32 @@ function getRandomQuote(){
 
 // printQuote builds a string from the getRandomQuote function and adds it to the DOM
 function printQuote(){
-  var result = getRandomQuote();
+  result = getRandomQuote();
   message = '<p class="quote" >' + result.quote + '</p>';
+  message += '<p class="source">' + result.source + optional() +'</p>';
+  //sets the content of the page to the string message
   document.getElementById('quote-box').innerHTML = message;
-  //console.log(message);
+  document.body.style.background = "url("+ result.img +")";
+  document.getElementById('loadQuote').style.backgroundColor = getRandomColor();
 }
-/*var index
 
-for(var i = 0; i < quotes.length; i++){
-  index = quotes[i];
-  for(var key in index){
-  console.log(index[key]);
-  }
+// the optional function returns either year or citation or both from the quote object if it exists
+function optional(){
+  var returnValue = '';
+  if (result.citation !== undefined) {
+   returnValue = '<span class="citation">' + result.citation + '</span>'
 }
-*/
+  if (result.year !== undefined) {
+    returnValue += '<span class="year">' + result.year + '</span>'
+  }
+  return returnValue
+}
+// this function returns a string with randomly generated hex-color
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
